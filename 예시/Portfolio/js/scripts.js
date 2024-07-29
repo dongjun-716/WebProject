717e061b0f085bc674856ef1089fcf2ee9627bcd
dongjun-716 <djk0716@naver.com>
2024-07-29 12:01:02 +0900

    day 4

--- 예시/Portfolio/js/scripts.js
+++ 예시/Portfolio/js/scripts.js
@@ -50,5 +50,23 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+    
 
 });
+
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const slideWidth = slides.children[0].clientWidth;
+    slides.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
+}
